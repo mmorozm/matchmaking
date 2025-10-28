5bbb1ebd31672a4d8338978a9b99f91a1f645e1b
mmoroz <mickaelfrost@gmail.com>
2025-10-28 19:09:26 +0200

    Add migrations for Reservations, Playlist updates, and Match state
    - Introduced `Reservations` table and related relationships.
    - Renamed `PLaylistType` to `PlaylistType` and added `Score` column to `Playlists`.
    - Updated `Match` entity with `PlaylistId` and `ScenarioId` columns and foreign keys.
    - Added `State` column to `Match` entity.

---
+++ src/Migrations/Matchmaking.Migrations.Data/Migrations/20251028153924_AddMatchReservations.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Matchmaking.Migrations.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMatchReservations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PLaylistType",
+                table: "Playlists",
+                newName: "PlaylistType");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "Playlists",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TicketId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MatchId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TeamId = table.Column<int>(type: "integer", nullable: false),
+                    Present = table.Column<bool>(type: "boolean", nullable: false),
+                    PresentChanged = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Registered = table.Column<bool>(type: "boolean", nullable: false),
+                    RegisteredChanged = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Matches_MatchId",
+                        column: x => x.MatchId,
+                        principalTable: "Matches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Tickets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_MatchId",
+                table: "Reservations",
+                column: "MatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_TicketId",
+                table: "Reservations",
+                column: "TicketId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "Playlists");
+
+            migrationBuilder.RenameColumn(
+                name: "PlaylistType",
+                table: "Playlists",
+                newName: "PLaylistType");
+        }
+    }
+}
---
+++ src/Migrations/Matchmaking.Migrations.Data/Migrations/20251028170232_UpdateMatchAddPlaylistAndScenarios.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Matchmaking.Migrations.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMatchAddPlaylistAndScenarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlaylistId",
+                table: "Matches",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ScenarioId",
+                table: "Matches",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_PlaylistId",
+                table: "Matches",
+                column: "PlaylistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_ScenarioId",
+                table: "Matches",
+                column: "ScenarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Playlists_PlaylistId",
+                table: "Matches",
+                column: "PlaylistId",
+                principalTable: "Playlists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Scenarios_ScenarioId",
+                table: "Matches",
+                column: "ScenarioId",
+                principalTable: "Scenarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Playlists_PlaylistId",
+                table: "Matches");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Scenarios_ScenarioId",
+                table: "Matches");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Matches_PlaylistId",
+                table: "Matches");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Matches_ScenarioId",
+                table: "Matches");
+
+            migrationBuilder.DropColumn(
+                name: "PlaylistId",
+                table: "Matches");
+
+            migrationBuilder.DropColumn(
+                name: "ScenarioId",
+                table: "Matches");
+        }
+    }
+}
